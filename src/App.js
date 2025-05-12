867b46b77c709024fd369559070028756fd2079e
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-05-12 22:38:26 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/homepage';
+import Homepage from './pages/homepage';
 import Features from './pages/features';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Homepage />} />
         <Route path="/features" element={<Features />} />
       </Routes>
     </Router>
