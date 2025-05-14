4276d83f5e3d37e55fc0867890a2e7a3a35aa5e6
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-05-14 10:39:26 +0530

    mobile view

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -156,12 +156,12 @@ const App = () => {
               </a>
             ))}
             <div className="px-4 py-4 border-t">
-              <a
-                href="#book-visit"
+              <Link 
+                to ="/signin"
                 className="flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
               >
-                Book a Visit
-              </a>
+                SignIn
+              </Link>
             </div>
           </div>
         </div>
