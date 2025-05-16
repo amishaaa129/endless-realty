43aefcc066a7a114dfb453a2d75592f750695322
amishaaa129 <amishawilliams2019@gmail.com>
2025-05-16 22:29:51 +0530

    connecting database for deployment

--- real-estate-backend/models/db.js
+++ real-estate-backend/models/db.js
@@ -1,6 +1,8 @@
 const { Pool } = require('pg');
+const URL = "postgresql://postgres:strangert3@localhost:5432/endless_realty" || process.env.DATABASE_URL;
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 module.exports = pool;
