5132507d47a2ef402f94eac4693e608fa22218aa
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-06-23 21:06:38 +0530

    nothing

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -166,6 +166,7 @@ const App = () => {
           </div>
         </div>
       )}
+      
 
       {/* Hero Section */}
       <div className="bg-gradient-to-b from-black/65 via-black/50 to-transparent bg-cover bg-center bg-fixed" style={{ backgroundImage: "url('https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80')" }}>
