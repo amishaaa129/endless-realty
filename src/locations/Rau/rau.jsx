ad051f6b197d87f30035333aed706c47f6056fff
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-06-26 14:04:59 +0530

    added rau

--- src/locations/Rau/rau.jsx
+++ src/locations/Rau/rau.jsx
@@ -1,22 +1,26 @@
-// File: vrindavan.jsx
+
 import React from 'react';
 import './rau.css';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../components/Header';
 import Footer from '../../components/footer';
 
-const Vrindavan = () => {
+const Rau = () => {
   const navigate = useNavigate();
 
   return (
     <div>
       <Header />
-      <div className="vrindavan-card">
+      <div className="Rau-card">
         <img
           src="/images/rau.jpeg"
-          alt="Vrindavan Property"
+          alt="Rau Property"
           className="property-image"
         />
+        {/* SOLD OUT Stamp */}
+        <div className="sold-out-tag">SOLD OUT</div>
+        
+        
         <h2 className="property-title">Silicon city property</h2>
         <button className="know-more-button" onClick={() => navigate('/rau-premium')}>
           Click to Know More
@@ -27,4 +31,4 @@ const Vrindavan = () => {
   );
 };
 
-export default Vrindavan;
+export default Rau;
