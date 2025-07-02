2b96bd6c06bc0f56052e9927c6a19f71cd823f43
ROHIT SINGHAL <singhals.rohit1@gmail.com>
2025-07-02 20:29:04 +0530

    seiufse

---
+++ src/locations/Shubham/arcadia/arcadia.jsx
@@ -0,0 +1,30 @@
+// File: vrindavan.jsx
+import React from 'react';
+import './vrindavan.css';
+import { useNavigate } from 'react-router-dom';
+import Header from '../../components/Header';
+import Footer from '../../components/footer';
+
+const Vrindavan = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Header />
+      <div className="vrindavan-card">
+        <img
+          src="/images/vrindavan.png"
+          alt="Vrindavan Property"
+          className="property-image"
+        />
+        <h2 className="property-title">Vrindavan Premium</h2>
+        <button className="know-more-button" onClick={() => navigate('/vrindavan-premium-row-houses')}>
+          Click to Know More
+        </button>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Vrindavan;
